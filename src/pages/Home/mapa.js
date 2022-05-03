edc0f93fdf74d0d5cdc3ac8a266e1583f3e13631
Márcio <85747327+MSA-program@users.noreply.github.com>
2022-05-03 09:29:04 -0300

    Implementação Mapa do Site
    Implementação Mapa do Site

--- src/pages/Home/mapa.js
+++ src/pages/Home/mapa.js
@@ -37,6 +37,108 @@ export default function Mapa() {
     <Link to='/cadastro-subcategoria'> Cadastro de Subcategoria </Link>
   </div>
 
+  <div className="btn">
+    <Link to='/cadastro-unidadedemedida'> Cadastro de Unidade de Medida </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/cadastro-produto'> Cadastro de Produto </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/cadastro-pessoafisica'> Cadastro de Pessoa Física </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/cadastro-pessoajuridica'> Cadastro de Pessoa Jurídica </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/cadastro-tipopagamento'> Cadastro de Tipo de Pagamento </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/cadastro-usuario'> Cadastro de Usuário </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/consulta-categoria'> Consulta de Categoria </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/consulta-subcategoria'> Consulta de Subcategoria </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/consulta-unidadedemedida'> Consulta de Unidade de Medida </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/consulta-produto'> Consulta de Produto </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/consulta-pessoafisica'> Consulta de Pessoa Física </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/consulta-pessoajuridica'> Consulta de Pessoa Jurídica </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/consulta-tipopagamento'> Consulta de Tipo de Pagamento </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/consulta-usuario'> Consulta de Usuário </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/consulta-operacao'> Consulta de Operação </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/consulta-pagamentocompra'> Consulta de Pagamento-Compra </Link>
+  </div>
+
+  <div className="btn">
+    <Link to='/consulta-pagamentovenda'> Consulta de Pagamento-Venda </Link>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   </div>
   </div>
   <div className="footer">
